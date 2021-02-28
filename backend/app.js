c46e6f3eb66ca9a6d08b1a732b30cf2e59165d31
admin <accsergey@gmail.com>
2021-02-28 08:15:59 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { celebrate, Joi } = require('celebrate');
@@ -25,7 +24,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 const jsonParser = bodyParser.json();
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 
 app.use(requestLogger);
 
