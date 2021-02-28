2c645cfc2d724c945b8440ae69954434ab369f83
admin <accsergey@gmail.com>
2021-02-28 14:06:36 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const urls = ['http://ggg.students.nomoredomains.icu/', 'https://http://www.ggg.students.nomoredomains.icu/', 'http://localhost:3000/'];
-
-app.use(cors({
-  origin: urls,
-  credentials: true,
-}));
+app.use(cors());
 
 const jsonParser = bodyParser.json();
 
